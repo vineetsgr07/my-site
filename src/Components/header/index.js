5ae3a97c2e1111412dae6cbd258fa39eab08901a
Vineet <vineet.sgr07@gmail.com>
2021-04-28 21:45:34 +0530

    added style

---
+++ src/Components/header/index.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+function App() {
+    return (
+        <header className="App">
+            Header
+            <div style={{ display: 'flex' }}>
+                <div> About Me </div>
+                <div> Projects </div>
+                <div> Resume </div>
+                <div> Articles </div>
+                <div></div>
+
+            </div>
+        </header>
+    );
+}
+
+export default App;
