2e086e62bcb85ac2503f2dc87f5bf25d4ebdbee0
Vineet <vineet.sgr07@gmail.com>
2021-04-30 00:02:37 +0530

    added content and footer

--- src/Components/content/index.js
+++ src/Components/content/index.js
@@ -1,3 +1,10 @@
 import React from "react";
 
 
+export const content = () => {
+
+    return <>
+        This is content
+    </>
+}
+
---
+++ src/Components/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export const footer = () => {
+    return <>
+        This is Footer
+    </>
+}
+
