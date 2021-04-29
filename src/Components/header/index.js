5a95fcac8131031492dead2579316e3e8678354d
Vineet <vineet.sgr07@gmail.com>
2021-04-30 00:03:26 +0530

    added content and footer

--- src/Components/header/index.js
+++ src/Components/header/index.js
@@ -4,12 +4,14 @@ function App() {
     return (
         <header className="App">
             Header
+             
             <div style={{ display: 'flex' }}>
                 <div> About Me </div>
                 <div> Projects </div>
                 <div> Resume </div>
                 <div> Articles </div>
                 <div></div>
+
 
             </div>
         </header>
